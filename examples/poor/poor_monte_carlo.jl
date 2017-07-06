59e16746187c5931a853ddae32da88bfff543652
James Schloss <james.schloss@oist.jp>
2017-07-06 13:15:44 +0900

    adding a poorly optimized monte carlo script.

---
+++ examples/poor/poor_monte_carlo.jl
@@ -0,0 +1,23 @@
+#-------------poor-julia.jl----------------------------------------------------#
+#
+# Purpose: This code is supposed to be slow in julia
+#
+#------------------------------------------------------------------------------#
+
+type Loc
+    x::Array{Float64}
+    y::Array{Float64}
+end
+
+# this integrates the area of a circle
+pos = Loc([0.0],[0.0])
+count = 0
+for i = 1:num
+    push!(pos.x, rand())
+    push!(pos.y, rand())
+    if (pos.x[i] * pos.x[i] + pos.y[i] * pos.y[i] < rad*rad)
+        count += 1.0
+    end
+end
+
+println("The area of your circle is: ", (count / num) * rad*rad*4)
